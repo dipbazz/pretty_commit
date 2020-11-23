0d8d1bd0327081e9e2d1f193b0989601313de903
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-23 15:58:06 +0545

    Update git.rb to read and handle missing file
    If the file doesn't exist read_file method handles with
    file donot exist message.
    Remove instance variable @file and directly calling read_file
    method.

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 require 'colorize'
-require './lib/git.rb'
-
+require './lib/git'
 
 Dir.chdir(__dir__) do
   Dir.chdir('../') do
     if Dir.exist?('.git')
-      x = Git.new("#{Dir.pwd}/TEST_COMMIT.txt")
+      x = Git.new("#{Dir.pwd}/CnOMMIT_EDITMSG")
+
+      puts x.title, x.description
+      puts x.full_title
     else
       puts 'You have not created a git repository. First create a repo and add commits.'.colorize(:red).underline
     end
--- lib/git.rb
+++ lib/git.rb
@@ -1,6 +1,7 @@
 class Git
-  def initialize(file)
-    @file = file
+  attr_reader :title, :description
+
+  def initialize(file_path)
     @title = {
       type: '',
       scope: '',
@@ -8,21 +9,37 @@ class Git
     }
     @description = ''
 
-    read_file do |line|
-      puts line
-    end
+    read_file(file_path)
+  end
+
+  def full_title
+    "#{title[:type]} #{title[:scope]} #{title[:short_summary]}"
   end
 
   private
 
-  def read_file
-    File.open(@file) do |file|
+  def read_file(file_path)
+    File.open(file_path) do |file|
+      got_title = false
       file.each do |line|
-        if line.include?("\n")
-          puts "here it is: " + line
+        if line.length > 1 && line.include?("\n")
+          if got_title
+            @description += line unless line.lstrip.match(/^#/)
+          else
+            update_title(line)
+            got_title = true
+          end
         end
-        yield line unless line.lstrip.match(/^#/)
       end
     end
+  rescue Errno::ENOENT
+    'File donot exist.'
+  end
+
+  def update_title(line)
+    title_splitted = line.split
+    title[:type] = title_splitted[0]
+    title[:scope] = title_splitted[1]
+    title[:short_summary] = title_splitted[2..-1].join(' ')
   end
 end
