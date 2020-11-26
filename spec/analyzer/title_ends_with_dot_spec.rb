dc728bdd4f729587c4a38251375e31f2aded85b4
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-26 19:32:35 +0545

    Test TitleEndsWithDot class public methods
    Testcase for method
    - #check_error when dont't have dot
      * returns empty
    - #check_error when have a dot
      * adds error

---
+++ spec/analyzer/title_ends_with_dot_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../../lib/analyzer/title_ends_with_dot'
+require_relative '../../lib/analyzer/base'
+
+describe TitleEndsWithDot do
+  let(:title_without_dot) { TitleEndsWithDot.new('A title without dot') }
+  let(:title_with_dot) { TitleEndsWithDot.new('This is a title with dot.') }
+
+  describe '#check_error' do
+    context "when don't have dot" do
+      it 'returns empty' do
+        expect(title_without_dot.check_error).to be_nil
+      end
+    end
+
+    context 'when title have a dot' do
+      it 'adds error' do
+        PrettyCommit::Error.clear
+        title_with_dot.check_error
+        expect(PrettyCommit::Error.all.length).to eql(1)
+      end
+    end
+  end
+end
