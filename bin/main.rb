541d765bca8b251872da7d08927ba6ec5d15f1c5
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-27 15:52:15 +0545

    Update main.rb and runner.rb files
    update main.rb file to take other arguments refactore usage message.
    new argument it takes:
    * -p <dir_name> this defines a path to directory to check
    * -c it executes a git commit cmd for user and provide them a commit
      template to guide a user.
    
    Update runner.rb file with a class Runner which have all previous
    methods encapsulated and some new methods #default_commit_msg_file
    , #check_for_error, and #report
    - #default_commit_msg_file provide a commit template file path which
      contains guidlines of commit to help user follow the commit standard
    - #check_for_error method checks error for all analyzer class that are
      initialized in @analyzer instance variable.
    - #report method produce a report for a linter to display.

--- bin/main.rb
+++ bin/main.rb
@@ -6,11 +6,21 @@ require_relative '../lib/runner'
 TAB_SIZE = 4
 
 if ARGV.include?('-h')
-  puts 'Usage: bin/PrettyCommit [path]'.colorize(:green)
-  puts "\npath:"
+  puts 'Usage: bin/main.rb [-p <dir_path>] [-c] [-h]'
+  puts "\nExplanation of arguments:"
+  puts "#{' ' * TAB_SIZE}-p #{' ' * TAB_SIZE} is to specify a path for a directory."
+  puts "#{' ' * TAB_SIZE}-c #{' ' * TAB_SIZE} is to specify if you want this program to run git commit for you."
+  puts "#{' ' * TAB_SIZE}-h #{' ' * TAB_SIZE} gives you this help mesage."
+  puts "\n<dir_path>:"
   puts "#{' ' * TAB_SIZE}1. /path/to/your/project/directory/"
   puts "#{' ' * TAB_SIZE}2. . (default)"
 else
-  dir_path = ARGV[0] || '.'
-  start_preety_commit(dir_path) { |message| puts message }
+  dir_path = '.'
+  if ARGV.include?('-p')
+    index = ARGV.find_index('-p')
+    dir_path = ARGV[index + 1] if ARGV[index + 1]
+  end
+  run = Runner.new(dir_path, commit: ARGV.include?('-c'))
+  run.start { |message| puts message }
+  # start_preety_commit(dir_path, ARGV.include?('-c')) { |message| puts message }
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -6,48 +6,75 @@ require_relative './analyzer/title_ends_with_dot'
 require_relative './analyzer/description'
 require_relative './analyzer/base'
 
-def access_commit_file
-  File.join(Dir.pwd, '/COMMIT_EDITMSG')
-end
+class Runner
+  def initialize(dir_path, commit: false)
+    @dir_path = dir_path
+    @commit = commit
 
-def create_last_commit_file
-  system('git log -n 1 > COMMIT_EDITMSG')
-end
-
-def display_total_error(total_error)
-  if total_error.zero?
-    yield "\nHmm. You have mastered the git commit message.".colorize(:green)
-  else
-    yield "\nTotal #{"#{total_error} errors".colorize(:red)} detected."
+    @git = nil
+    @analyzer = []
   end
-end
 
-def start_preety_commit(dir_path, &block)
-  Dir.chdir(dir_path) do
-    if Dir.exist?('.git')
-      create_last_commit_file
+  def start(&block)
+    Dir.chdir(@dir_path) do
+      if Dir.exist?('.git')
+        create_commit(default_commit_msg_file) if @commit
 
-      git_commit = Git.new(access_commit_file)
+        unless @commit
+          create_last_commit_file
 
-      TitleLength.new(git_commit.title.full_title).check_error
+          @git = Git.new(commit_file)
+          @analyzer = [
+            TitleLength.new(@git.title.full_title),
+            TitleTypeValid.new(@git.title.type),
+            TitleEndsWithDot.new(@git.title.full_title),
+            DescriptionAnalyzer.new(@git.description.description)
+          ]
 
-      TitleTypeValid.new(git_commit.title.type).check_error
-
-      TitleEndsWithDot.new(git_commit.title.full_title).check_error
-
-      TitleTypeCapitalize.new(git_commit.title.type).check_error
-
-      DescriptionAnalyzer.new(git_commit.description.description).check_error
-
-      total_error = 0
-      ObjectSpace.each_object(PrettyCommit::Error).each do |instance|
-        instance.generate_error(&block)
-        total_error += instance.total_error
+          check_for_error
+          report(&block)
+        end
+      else
+        yield 'You have not created a git repository. First create a repo and add commit.'.colorize(:red).underline
       end
+    end
+  end
 
-      display_total_error(total_error, &block)
+  def commit_file
+    File.join(__dir__, '../COMMIT_EDITMSG')
+  end
+
+  def default_commit_msg_file
+    File.join(__dir__, '../GIT_COMMIT_MSG')
+  end
+
+  def create_last_commit_file
+    system("git log -n 1 > #{commit_file}")
+  end
+
+  def create_commit(file_path)
+    system("git commit -t #{file_path}")
+  end
+
+  def check_for_error
+    @analyzer.each(&:check_error)
+  end
+
+  def report(&block)
+    total_error = 0
+    ObjectSpace.each_object(PrettyCommit::Error).each do |instance|
+      instance.generate_error(&block)
+      total_error += instance.total_error
+    end
+
+    total_error(total_error, &block)
+  end
+
+  def total_error(total_error)
+    if total_error.zero?
+      yield "\nHmm. You have mastered the git commit message.".colorize(:green)
     else
-      yield 'You have not created a git repository. First create a repo and add commits.'.colorize(:red).underline
+      yield "\nTotal #{"#{total_error} errors".colorize(:red)} detected."
     end
   end
 end
